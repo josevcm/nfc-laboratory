5097e099e317f8ab878c48f1a473bccc024c2f88
jvcampos <jvcampos@indra.es>
2021-09-18 12:34:39 +0200

    logging support to file stream, but not enabled by default

--- src/nfc-lib/lib-rt/rt-lang/src/main/cpp/Logger.cpp
+++ src/nfc-lib/lib-rt/rt-lang/src/main/cpp/Logger.cpp
@@ -26,6 +26,7 @@
 #include <pthread.h>
 
 #include <string>
+#include <fstream>
 #include <cmath>
 #include <thread>
 #include <utility>
@@ -37,8 +38,9 @@
 #include <rt/BlockingQueue.h>
 
 //#define NULL_LOG
-//#define DIRECT_LOG
-#define THREADED_LOG
+//#define STDERR_LOG
+#define STDOUT_LOG
+//#define FSTREAM_LOG
 
 namespace rt {
 
@@ -62,6 +64,7 @@ const char *tags[] = {
       "TRACE" // 16
 };
 
+// log event for store debugging information
 struct LogEvent
 {
    std::string level;
@@ -83,24 +86,27 @@ struct LogEvent
    }
 };
 
+// null logger for discard all messages
 #ifdef NULL_LOG
-struct LogThread
+struct LogWriter
 {
    void push(LogEvent *event)
    {
+      delete event;
    }
 } writer;
 #endif
 
-#ifdef DIRECT_LOG
-struct LogThread
+// direct to stderr logger, waring: big performance impact! use only for strange debugging when others logger do not work!
+#ifdef STDERR_LOG
+struct LogWriter
 {
    void push(LogEvent *event)
    {
-      print(std::shared_ptr<LogEvent>(event));
+      write(std::shared_ptr<LogEvent>(event));
    }
 
-   static void print(const std::shared_ptr<LogEvent> &event)
+   void write(const std::shared_ptr<LogEvent> &event)
    {
       char date[32];
       struct tm timeinfo {};
@@ -113,13 +119,16 @@ struct LogThread
       strftime(date, sizeof(date), "%Y-%m-%d %H:%M:%S", &timeinfo);
 
       fprintf(stderr,"%s.%03d (thread-%d) %s [%s] %s\n", date, millis, event->thread, event->level.c_str(), event->logger.c_str(), Format::format(event->format, event->params).c_str());
+
+      delete event;
    }
 
 } writer;
 #endif
 
-#ifdef THREADED_LOG
-struct LogThread
+// threaded logger to console stdout, runs on low priority thread
+#ifdef STDOUT_LOG
+struct LogWriter
 {
    // writer thread
    std::thread thread;
@@ -128,9 +137,9 @@ struct LogThread
    std::atomic<bool> shutdown;
 
    // events queue
-   BlockingQueue<std::shared_ptr<LogEvent>> queue;
+   BlockingQueue<LogEvent *> queue;
 
-   LogThread() : thread([this] { this->exec(); }), shutdown(false)
+   LogWriter() : thread([this] { this->exec(); }), shutdown(false)
    {
       sched_param param {0};
 
@@ -140,7 +149,7 @@ struct LogThread
       }
    }
 
-   ~LogThread()
+   ~LogWriter()
    {
       // signal shutdown
       shutdown = true;
@@ -160,12 +169,12 @@ struct LogThread
       {
          while (auto event = queue.get(50))
          {
-            print(event.value());
+            write(event.value());
          }
       }
    }
 
-   static void print(const std::shared_ptr<LogEvent> &event)
+   void write(LogEvent *event)
    {
       char date[32];
       struct tm timeinfo {};
@@ -178,6 +187,87 @@ struct LogThread
       strftime(date, sizeof(date), "%Y-%m-%d %H:%M:%S", &timeinfo);
 
       fprintf(stdout, "%s.%03d (thread-%d) %s [%s] %s\n", date, millis, event->thread, event->level.c_str(), event->logger.c_str(), Format::format(event->format, event->params).c_str());
+
+      delete event;
+   }
+
+} writer;
+#endif
+
+// threaded logger to file stream, runs on low priority thread
+#ifdef FSTREAM_LOG
+struct LogWriter
+{
+   // writer thread
+   std::thread thread;
+
+   // shutdown flag
+   std::atomic<bool> shutdown;
+
+   // output file
+   std::ofstream stream;
+
+   // events queue
+   BlockingQueue<std::shared_ptr<LogEvent>> queue;
+
+   LogWriter() : thread([this] { this->exec(); }), shutdown(false), stream("log/nfc-lab.log", std::ios::out | std::ios::app)
+   {
+      sched_param param {0};
+
+      if (pthread_setschedparam(thread.native_handle(), SCHED_OTHER, &param))
+      {
+         printf("error setting logger thread priority: %s\n", std::strerror(errno));
+      }
+   }
+
+   ~LogWriter()
+   {
+      // close file
+      stream.close();
+
+      // signal shutdown
+      shutdown = true;
+
+      // wait for thread to finish
+      thread.join();
+   }
+
+   void push(LogEvent *event)
+   {
+      queue.add(event);
+   }
+
+   void exec()
+   {
+      while (!shutdown)
+      {
+         while (auto event = queue.get(50))
+         {
+            if (stream)
+            {
+               write(event.value());
+            }
+         }
+      }
+   }
+
+   void write(LogEvent *event)
+   {
+      struct tm timeinfo {};
+      char date[32], buffer[4096];
+
+      auto seconds = std::chrono::duration_cast<std::chrono::seconds>(event->time.time_since_epoch()).count();
+      auto millis = std::chrono::duration_cast<std::chrono::milliseconds>(event->time.time_since_epoch()).count() % 1000;
+
+      localtime_s(&timeinfo, &seconds);
+
+      strftime(date, sizeof(date), "%Y-%m-%d %H:%M:%S", &timeinfo);
+
+      snprintf(buffer, sizeof(buffer), "%s.%03d (thread-%d) %s [%s] %s\n", date, millis, event->thread, event->level.c_str(), event->logger.c_str(), Format::format(event->format, event->params).c_str());
+
+      stream << buffer;
+
+      delete event;
    }
 
 } writer;
