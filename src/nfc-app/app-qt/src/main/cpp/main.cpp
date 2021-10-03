240350124415dac6581ae4c43fa761d2b035982c
jvcampos <jvcampos@indra.es>
2021-10-03 22:54:06 +0200

    NfcB parser

--- src/nfc-app/app-qt/src/main/cpp/main.cpp
+++ src/nfc-app/app-qt/src/main/cpp/main.cpp
@@ -142,7 +142,7 @@ int startApp(int argc, char *argv[])
 
 int main(int argc, char *argv[])
 {
-   return startTest(argc, argv);
-//   return startApp(argc, argv);
+//   return startTest(argc, argv);
+   return startApp(argc, argv);
 }
 
