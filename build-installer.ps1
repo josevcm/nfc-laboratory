a6efa6dcdb829b15551cb951b367fbb675640da7
Jose Vicente Campos <josevcm@gmail.com>
2021-11-22 19:24:42 +0100

    prepare v2.6.0

--- build-installer.ps1
+++ build-installer.ps1
@@ -13,9 +13,7 @@ Copy-Item -Force -Recurse dat/conf   $BuildPath/packages/nfc-lab/data
 Copy-Item -Force -Recurse dat/fonts  $BuildPath/packages/nfc-lab/data
 
 # Copy application external libraries
-Copy-Item -Force dll/glew-2.1.0/x86_64-w64-mingw32/bin/*.dll      $BuildPath/packages/nfc-lab/data/
 Copy-Item -Force dll/usb-1.0.20/x86_64-w64-mingw32/bin/*.dll      $BuildPath/packages/nfc-lab/data/
-Copy-Item -Force dll/freetype-2.11.0/x86_64-w64-mingw32/bin/*.dll $BuildPath/packages/nfc-lab/data/
 
 # Copy application executable
 Copy-Item -Force cmake-build-release/src/nfc-app/app-qt/nfc-lab.exe $BuildPath/packages/nfc-lab/data/
@@ -25,4 +23,4 @@ windeployqt.exe --debug --force --compiler-runtime --no-translations --no-system
 # windeployqt.exe --release --force --compiler-runtime --no-translations --no-system-d3d-compiler --no-angle --no-opengl-sw $BuildPath/packages/nfc-lab/data/nfc-lab.exe
 
 # Create QT installer
-binarycreator.exe -c $BuildPath/config/config.xml -p $BuildPath/packages $BuildPath/nfc-lab-2.5.0-x86_64.exe
+binarycreator.exe -c $BuildPath/config/config.xml -p $BuildPath/packages $BuildPath/nfc-lab-2.6.0-x86_64.exe
--- build-runenv.ps1
+++ build-runenv.ps1
@@ -8,9 +8,7 @@ if (Test-Path "$BuildDebugPath/nfc-lab.exe") {
     Copy-Item -Force -Recurse dat/fonts  $BuildDebugPath
 
     # Copy application external libraries
-    Copy-Item -Force dll/glew-2.1.0/x86_64-w64-mingw32/bin/*.dll      $BuildDebugPath
     Copy-Item -Force dll/usb-1.0.20/x86_64-w64-mingw32/bin/*.dll      $BuildDebugPath
-    Copy-Item -Force dll/freetype-2.11.0/x86_64-w64-mingw32/bin/*.dll $BuildDebugPath
 
     windeployqt.exe --debug --compiler-runtime --no-translations --no-system-d3d-compiler --no-angle --no-opengl-sw $BuildDebugPath/nfc-lab.exe
 }
@@ -22,9 +20,7 @@ if (Test-Path "$BuildReleasePath/nfc-lab.exe") {
     Copy-Item -Force -Recurse dat/fonts  $BuildReleasePath
 
     # Copy application external libraries
-    Copy-Item -Force dll/glew-2.1.0/x86_64-w64-mingw32/bin/*.dll      $BuildReleasePath
     Copy-Item -Force dll/usb-1.0.20/x86_64-w64-mingw32/bin/*.dll      $BuildReleasePath
-    Copy-Item -Force dll/freetype-2.11.0/x86_64-w64-mingw32/bin/*.dll $BuildReleasePath
 
     windeployqt.exe --debug --compiler-runtime --no-translations --no-system-d3d-compiler --no-angle --no-opengl-sw $BuildReleasePath/nfc-lab.exe
 }
