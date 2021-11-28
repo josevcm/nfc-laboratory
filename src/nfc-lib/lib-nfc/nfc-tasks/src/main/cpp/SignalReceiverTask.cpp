3e09ed37c28c28abca806420e77f4e8a78176fea
Jose Vicente Campos <jvcampos@indra.es>
2021-11-28 14:53:18 +0100

    performance throughput measure

--- src/nfc-lib/lib-nfc/nfc-tasks/src/main/cpp/SignalReceiverTask.cpp
+++ src/nfc-lib/lib-nfc/nfc-tasks/src/main/cpp/SignalReceiverTask.cpp
@@ -22,9 +22,16 @@
 
 */
 
+#ifdef __SSE2__
+
+#include <x86intrin.h>
+
+#endif
+
 #include <rt/Logger.h>
 #include <rt/Format.h>
 #include <rt/BlockingQueue.h>
+#include <rt/Throughput.h>
 
 #include <sdr/SignalType.h>
 #include <sdr/SignalBuffer.h>
@@ -33,10 +40,6 @@
 #include <nfc/SignalReceiverTask.h>
 
 #include "AbstractTask.h"
-
-#ifdef __SSE2__
-#include <x86intrin.h>
-#endif
 
 namespace nfc {
 
@@ -56,6 +59,9 @@ struct SignalReceiverTask::Impl : SignalReceiverTask, AbstractTask
 
    // signal stream queue buffer
    rt::BlockingQueue<sdr::SignalBuffer> signalQueue;
+
+   // throughput meter
+   rt::Throughput taskThroughput;
 
    // last detection attempt
    std::chrono::time_point<std::chrono::steady_clock> lastSearch;
@@ -119,9 +125,14 @@ struct SignalReceiverTask::Impl : SignalReceiverTask, AbstractTask
       /*
       * process device refresh
       */
-      if ((std::chrono::steady_clock::now() - lastSearch) > std::chrono::milliseconds(500))
+      if ((std::chrono::steady_clock::now() - lastSearch) > std::chrono::milliseconds(1000))
       {
          refresh();
+
+         if (receiver && receiver->isStreaming())
+         {
+            log.info("average throughput {.2} Msps", {taskThroughput.average() / 1E6});
+         }
       }
 
       processQueue(50);
@@ -365,43 +376,65 @@ struct SignalReceiverTask::Impl : SignalReceiverTask, AbstractTask
          float *dst = result.pull(buffer.elements());
          float avrg = 0;
 
+         taskThroughput.begin();
+
          // compute real signal value and average value
 #ifdef __SSE2__
-         for (int j = 0, n = 0; j < buffer.elements(); j += 8, n += 16)
+         for (int j = 0, n = 0; j < buffer.elements(); j += 16, n += 32)
          {
-            // load 8 I/Q vectors
-            __m128 a1 = _mm_loadu_ps(src + n + 0);  // I0, Q0, I1, Q1
-            __m128 a2 = _mm_loadu_ps(src + n + 4);  // I2, Q2, I3, Q3
-            __m128 a3 = _mm_loadu_ps(src + n + 8);  // I4, Q4, I5, Q5
-            __m128 a4 = _mm_loadu_ps(src + n + 12); // I6, Q6, I7, Q7
+            // load 16 I/Q vectors
+            __m128 a0 = _mm_loadu_ps(src + n + 0);  // I0, Q0, I1, Q1
+            __m128 a1 = _mm_loadu_ps(src + n + 4);  // I2, Q2, I3, Q3
+            __m128 a2 = _mm_loadu_ps(src + n + 8);  // I4, Q4, I5, Q5
+            __m128 a3 = _mm_loadu_ps(src + n + 12); // I6, Q6, I7, Q7
+            __m128 a4 = _mm_loadu_ps(src + n + 16);  // I8, Q8, I9, Q9
+            __m128 a5 = _mm_loadu_ps(src + n + 20); // I10, Q10, I11, Q11
+            __m128 a6 = _mm_loadu_ps(src + n + 24);  // I12, Q12, I13, Q13
+            __m128 a7 = _mm_loadu_ps(src + n + 28); // I14, Q14, I15, Q15
 
             // square all components
-            __m128 p1 = _mm_mul_ps(a1, a1); // I0^2, Q0^2, I1^2, Q1^2
-            __m128 p2 = _mm_mul_ps(a2, a2); // I2^2, Q2^2, I3^2, Q3^2
-            __m128 p3 = _mm_mul_ps(a3, a3); // I4^2, Q4^2, I5^2, Q5^2
-            __m128 p4 = _mm_mul_ps(a4, a4); // I6^2, Q6^2, I6^2, Q7^2
+            __m128 p0 = _mm_mul_ps(a0, a0); // I0^2, Q0^2, I1^2, Q1^2
+            __m128 p1 = _mm_mul_ps(a1, a1); // I2^2, Q2^2, I3^2, Q3^2
+            __m128 p2 = _mm_mul_ps(a2, a2); // I4^2, Q4^2, I5^2, Q5^2
+            __m128 p3 = _mm_mul_ps(a3, a3); // I6^2, Q6^2, I7^2, Q7^2
+            __m128 p4 = _mm_mul_ps(a4, a4); // I8^2, Q8^2, I9^2, Q9^2
+            __m128 p5 = _mm_mul_ps(a5, a5); // I10^2, Q10^2, I11^2, Q11^2
+            __m128 p6 = _mm_mul_ps(a6, a6); // I12^2, Q12^2, I13^2, Q13^2
+            __m128 p7 = _mm_mul_ps(a7, a7); // I14^2, Q14^2, I15^2, Q15^2
 
             // permute components
-            __m128 i1 = _mm_shuffle_ps(p1, p2, _MM_SHUFFLE(2, 0, 2, 0)); // I0^2, I1^2, I2^2, I3^2
-            __m128 i2 = _mm_shuffle_ps(p3, p4, _MM_SHUFFLE(2, 0, 2, 0)); // I4^2, I5^2, I6^2, I7^2
-            __m128 q1 = _mm_shuffle_ps(p1, p2, _MM_SHUFFLE(3, 1, 3, 1)); // Q0^2, Q1^2, Q2^2, Q3^2
-            __m128 q2 = _mm_shuffle_ps(p3, p4, _MM_SHUFFLE(3, 1, 3, 1)); // Q4^2, Q5^2, Q6^2, Q7^2
+            __m128 i0 = _mm_shuffle_ps(p0, p1, _MM_SHUFFLE(2, 0, 2, 0)); // I0^2, I1^2, I2^2, I3^2
+            __m128 i1 = _mm_shuffle_ps(p2, p3, _MM_SHUFFLE(2, 0, 2, 0)); // I4^2, I5^2, I6^2, I7^2
+            __m128 i2 = _mm_shuffle_ps(p4, p5, _MM_SHUFFLE(2, 0, 2, 0)); // I8^2, I9^2, I10^2, I11^2
+            __m128 i3 = _mm_shuffle_ps(p6, p7, _MM_SHUFFLE(2, 0, 2, 0)); // I12^2, I13^2, I14^2, I15^2
+            __m128 q0 = _mm_shuffle_ps(p0, p1, _MM_SHUFFLE(3, 1, 3, 1)); // Q0^2, Q1^2, Q2^2, Q3^2
+            __m128 q1 = _mm_shuffle_ps(p2, p3, _MM_SHUFFLE(3, 1, 3, 1)); // Q4^2, Q5^2, Q6^2, Q7^2
+            __m128 q2 = _mm_shuffle_ps(p4, p5, _MM_SHUFFLE(3, 1, 3, 1)); // Q8^2, Q9^2, Q10^2, Q11^2
+            __m128 q3 = _mm_shuffle_ps(p6, p7, _MM_SHUFFLE(3, 1, 3, 1)); // Q12^2, Q13^2, Q14^2, Q15^2
 
             // add vector components
-            __m128 r1 = _mm_add_ps(i1, q1); // I0^2+Q0^2, I1^2+Q1^2, I2^2+Q2^2, I3^2+Q3^2
-            __m128 r2 = _mm_add_ps(i2, q2); // I4^2+Q4^2, I5^2+Q5^2, I6^2+Q6^2, I7^2+Q7^2
+            __m128 r0 = _mm_add_ps(i0, q0); // I0^2+Q0^2, I1^2+Q1^2, I2^2+Q2^2, I3^2+Q3^2
+            __m128 r1 = _mm_add_ps(i1, q1); // I4^2+Q4^2, I5^2+Q5^2, I6^2+Q6^2, I7^2+Q7^2
+            __m128 r2 = _mm_add_ps(i2, q2); // I8^2+Q8^2, I9^2+Q1^2, I10^2+Q10^2, I11^2+Q11^2
+            __m128 r3 = _mm_add_ps(i3, q3); // I12^2+Q12^2, I13^2+Q13^2, I14^2+Q14^2, I15^2+Q15^2
 
             // square-root vectors
-            __m128 m1 = _mm_sqrt_ps(r1); // sqrt(I0^2+Q0^2), sqrt(I1^2+Q1^2), sqrt(I2^2+Q2^2), sqrt(I3^2+Q3^2)
-            __m128 m2 = _mm_sqrt_ps(r2); // sqrt(I4^2+Q4^2), sqrt(I5^2+Q5^2), sqrt(I6^2+Q6^2), sqrt(I7^2+Q7^2)
+            __m128 m0 = _mm_sqrt_ps(r0); // sqrt(I0^2+Q0^2), sqrt(I1^2+Q1^2), sqrt(I2^2+Q2^2), sqrt(I3^2+Q3^2)
+            __m128 m1 = _mm_sqrt_ps(r1); // sqrt(I4^2+Q4^2), sqrt(I5^2+Q5^2), sqrt(I6^2+Q6^2), sqrt(I7^2+Q7^2)
+            __m128 m2 = _mm_sqrt_ps(r2); // sqrt(I8^2+Q8^2), sqrt(I9^2+Q9^2), sqrt(I10^2+Q10^2), sqrt(I11^2+Q11^2)
+            __m128 m3 = _mm_sqrt_ps(r3); // sqrt(I12^2+Q12^2), sqrt(I13^2+Q13^2), sqrt(I14^2+Q14^2), sqrt(I15^2+Q15^2)
 
             // store results
-            _mm_storeu_ps(dst + j + 0, m1);
-            _mm_storeu_ps(dst + j + 4, m2);
+            _mm_storeu_ps(dst + j + 0, m0);
+            _mm_storeu_ps(dst + j + 4, m1);
+            _mm_storeu_ps(dst + j + 8, m2);
+            _mm_storeu_ps(dst + j + 12, m3);
 
             // compute exponential average
-            avrg = avrg * (1 - 0.001f) + dst[j+0] * 0.001f;
-            avrg = avrg * (1 - 0.001f) + dst[j+4] * 0.001f;
+            avrg = avrg * (1 - 0.001f) + dst[j + 0] * 0.001f;
+            avrg = avrg * (1 - 0.001f) + dst[j + 4] * 0.001f;
+            avrg = avrg * (1 - 0.001f) + dst[j + 8] * 0.001f;
+            avrg = avrg * (1 - 0.001f) + dst[j + 12] * 0.001f;
          }
 #else
 #pragma GCC ivdep
@@ -415,6 +448,8 @@ struct SignalReceiverTask::Impl : SignalReceiverTask, AbstractTask
             avrg = avrg * (1 - 0.001f) + dst[j + 0] * 0.001f;
          }
 #endif
+
+         taskThroughput.update(buffer.elements());
 
          // flip buffer pointers
          result.flip();
---
+++ src/nfc-lib/lib-rt/rt-lang/src/main/include/rt/Throughput.h
@@ -0,0 +1,83 @@
+/*
+
+  Copyright (c) 2021 Jose Vicente Campos Martinez - <josevcm@gmail.com>
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+
+*/
+
+#ifndef NFC_LAB_THROUGHPUT_H
+#define NFC_LAB_THROUGHPUT_H
+
+#include <chrono>
+
+namespace rt {
+
+class Throughput
+{
+   private:
+
+      // average time
+      double a = 0;
+
+      // average throughput
+      double r = 0;
+
+      // elapsed time
+      std::chrono::microseconds e;
+
+      // start time
+      std::chrono::steady_clock::time_point t;
+
+   public:
+
+      inline void begin()
+      {
+         t = std::chrono::steady_clock::now();
+      }
+
+      inline void update(double elements = 1)
+      {
+         // calculate elapsed time since start
+         auto s = std::chrono::steady_clock::now() - t;
+
+         // get elapsed time in microseconds
+         e = std::chrono::duration_cast<std::chrono::microseconds>(s);
+
+         // process exponential average time
+         a = a * (1 - 0.01) + double(e.count()) * 0.01;
+
+         // process exponential average throughput
+         r = r * (1 - 0.01) + (elements / double(e.count()) * 1E6) * 0.01;
+      }
+
+      inline double elapsed() const
+      {
+         return a;
+      }
+
+      inline double average() const
+      {
+         return r;
+      }
+};
+
+}
+
+#endif //NFC_LAB_THROUGHPUT_H
