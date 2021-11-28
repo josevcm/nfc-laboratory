744f856cfc0efbdf4e673d549e1aa8904f5d4f66
Jose Vicente Campos <jvcampos@indra.es>
2021-11-28 14:53:18 +0100

    performance throughput measure

--- src/nfc-lib/lib-nfc/nfc-tasks/src/main/cpp/FourierProcessTask.cpp
+++ src/nfc-lib/lib-nfc/nfc-tasks/src/main/cpp/FourierProcessTask.cpp
@@ -23,7 +23,9 @@
 */
 
 #ifdef __SSE2__
+
 #include <x86intrin.h>
+
 #endif
 
 #include <fft.h>
@@ -185,46 +187,64 @@ struct FourierProcessTask::Impl : FourierProcessTask, AbstractTask
 
          // transform complex FFT to real
 #ifdef __SSE2__
-         for (int i = 0, w = 0; w < length; i += 16, w += 8)
+         for (int i = 0, n = 0; i < length; i += 16, n += 32)
          {
-            // load 8 I/Q vectors
-            __m128 a1 = _mm_loadu_ps(fftOut + i + 0);  // r0, i0, r1, i1
-            __m128 a2 = _mm_loadu_ps(fftOut + i + 4);  // r2, i2, r3, i3
-            __m128 a3 = _mm_loadu_ps(fftOut + i + 8);  // r4, i4, r5, i5
-            __m128 a4 = _mm_loadu_ps(fftOut + i + 12); // r6, i6, r7, i7
+            // load 16 I/Q vectors
+            __m128 a0 = _mm_loadu_ps(fftOut + n + 0);  // I0, Q0, I1, Q1
+            __m128 a1 = _mm_loadu_ps(fftOut + n + 4);  // I2, Q2, I3, Q3
+            __m128 a2 = _mm_loadu_ps(fftOut + n + 8);  // I4, Q4, I5, Q5
+            __m128 a3 = _mm_loadu_ps(fftOut + n + 12); // I6, Q6, I7, Q7
+            __m128 a4 = _mm_loadu_ps(fftOut + n + 16);  // I8, Q8, I9, Q9
+            __m128 a5 = _mm_loadu_ps(fftOut + n + 20); // I10, Q10, I11, Q11
+            __m128 a6 = _mm_loadu_ps(fftOut + n + 24);  // I12, Q12, I13, Q13
+            __m128 a7 = _mm_loadu_ps(fftOut + n + 28); // I14, Q14, I15, Q15
 
             // square all components
-            __m128 p1 = _mm_mul_ps(a1, a1); // r0^2, Q0^2, r1^2, Q1^2
-            __m128 p2 = _mm_mul_ps(a2, a2); // r2^2, Q2^2, r3^2, Q3^2
-            __m128 p3 = _mm_mul_ps(a3, a3); // r4^2, Q4^2, r5^2, Q5^2
-            __m128 p4 = _mm_mul_ps(a4, a4); // r6^2, Q6^2, r6^2, Q7^2
+            __m128 p0 = _mm_mul_ps(a0, a0); // I0^2, Q0^2, I1^2, Q1^2
+            __m128 p1 = _mm_mul_ps(a1, a1); // I2^2, Q2^2, I3^2, Q3^2
+            __m128 p2 = _mm_mul_ps(a2, a2); // I4^2, Q4^2, I5^2, Q5^2
+            __m128 p3 = _mm_mul_ps(a3, a3); // I6^2, Q6^2, I7^2, Q7^2
+            __m128 p4 = _mm_mul_ps(a4, a4); // I8^2, Q8^2, I9^2, Q9^2
+            __m128 p5 = _mm_mul_ps(a5, a5); // I10^2, Q10^2, I11^2, Q11^2
+            __m128 p6 = _mm_mul_ps(a6, a6); // I12^2, Q12^2, I13^2, Q13^2
+            __m128 p7 = _mm_mul_ps(a7, a7); // I14^2, Q14^2, I15^2, Q15^2
 
             // permute components
-            __m128 i1 = _mm_shuffle_ps(p1, p2, _MM_SHUFFLE(2, 0, 2, 0)); // r0^2, r1^2, r2^2, r3^2
-            __m128 i2 = _mm_shuffle_ps(p3, p4, _MM_SHUFFLE(2, 0, 2, 0)); // r4^2, r5^2, r6^2, r7^2
-            __m128 q1 = _mm_shuffle_ps(p1, p2, _MM_SHUFFLE(3, 1, 3, 1)); // Q0^2, Q1^2, Q2^2, Q3^2
-            __m128 q2 = _mm_shuffle_ps(p3, p4, _MM_SHUFFLE(3, 1, 3, 1)); // Q4^2, Q5^2, Q6^2, Q7^2
+            __m128 i0 = _mm_shuffle_ps(p0, p1, _MM_SHUFFLE(2, 0, 2, 0)); // I0^2, I1^2, I2^2, I3^2
+            __m128 i1 = _mm_shuffle_ps(p2, p3, _MM_SHUFFLE(2, 0, 2, 0)); // I4^2, I5^2, I6^2, I7^2
+            __m128 i2 = _mm_shuffle_ps(p4, p5, _MM_SHUFFLE(2, 0, 2, 0)); // I8^2, I9^2, I10^2, I11^2
+            __m128 i3 = _mm_shuffle_ps(p6, p7, _MM_SHUFFLE(2, 0, 2, 0)); // I12^2, I13^2, I14^2, I15^2
+            __m128 q0 = _mm_shuffle_ps(p0, p1, _MM_SHUFFLE(3, 1, 3, 1)); // Q0^2, Q1^2, Q2^2, Q3^2
+            __m128 q1 = _mm_shuffle_ps(p2, p3, _MM_SHUFFLE(3, 1, 3, 1)); // Q4^2, Q5^2, Q6^2, Q7^2
+            __m128 q2 = _mm_shuffle_ps(p4, p5, _MM_SHUFFLE(3, 1, 3, 1)); // Q8^2, Q9^2, Q10^2, Q11^2
+            __m128 q3 = _mm_shuffle_ps(p6, p7, _MM_SHUFFLE(3, 1, 3, 1)); // Q12^2, Q13^2, Q14^2, Q15^2
 
             // add vector components
-            __m128 r1 = _mm_add_ps(i1, q1); // r0^2+Q0^2, r1^2+Q1^2, r2^2+Q2^2, r3^2+Q3^2
-            __m128 r2 = _mm_add_ps(i2, q2); // r4^2+Q4^2, r5^2+Q5^2, r6^2+Q6^2, r7^2+Q7^2
+            __m128 r0 = _mm_add_ps(i0, q0); // I0^2+Q0^2, I1^2+Q1^2, I2^2+Q2^2, I3^2+Q3^2
+            __m128 r1 = _mm_add_ps(i1, q1); // I4^2+Q4^2, I5^2+Q5^2, I6^2+Q6^2, I7^2+Q7^2
+            __m128 r2 = _mm_add_ps(i2, q2); // I8^2+Q8^2, I9^2+Q1^2, I10^2+Q10^2, I11^2+Q11^2
+            __m128 r3 = _mm_add_ps(i3, q3); // I12^2+Q12^2, I13^2+Q13^2, I14^2+Q14^2, I15^2+Q15^2
 
             // square-root vectors
-            __m128 m1 = _mm_sqrt_ps(r1); // sqrt(I0^2+Q0^2), sqrt(I1^2+Q1^2), sqrt(I2^2+Q2^2), sqrt(I3^2+Q3^2)
-            __m128 m2 = _mm_sqrt_ps(r2); // sqrt(I4^2+Q4^2), sqrt(I5^2+Q5^2), sqrt(I6^2+Q6^2), sqrt(I7^2+Q7^2)
+            __m128 m0 = _mm_sqrt_ps(r0); // sqrt(I0^2+Q0^2), sqrt(I1^2+Q1^2), sqrt(I2^2+Q2^2), sqrt(I3^2+Q3^2)
+            __m128 m1 = _mm_sqrt_ps(r1); // sqrt(I4^2+Q4^2), sqrt(I5^2+Q5^2), sqrt(I6^2+Q6^2), sqrt(I7^2+Q7^2)
+            __m128 m2 = _mm_sqrt_ps(r2); // sqrt(I8^2+Q8^2), sqrt(I9^2+Q9^2), sqrt(I10^2+Q10^2), sqrt(I11^2+Q11^2)
+            __m128 m3 = _mm_sqrt_ps(r3); // sqrt(I12^2+Q12^2), sqrt(I13^2+Q13^2), sqrt(I14^2+Q14^2), sqrt(I15^2+Q15^2)
 
             // store results
-            _mm_storeu_ps(fftMag + w + 0, m1);
-            _mm_storeu_ps(fftMag + w + 4, m2);
+            _mm_storeu_ps(fftMag + i + 0, m0);
+            _mm_storeu_ps(fftMag + i + 4, m1);
+            _mm_storeu_ps(fftMag + i + 8, m2);
+            _mm_storeu_ps(fftMag + i + 12, m3);
          }
 #else
 #pragma GCC ivdep
-         for (int i = 0, w = 0; w < length; i += 8, w += 4)
+         for (int i = 0, n = 0; i < length; i += 4, n += 8)
          {
-            fftMag[w + 0] = std::sqrt(fftOut[i + 0] * fftOut[i + 0] + fftOut[i + 1] * fftOut[i + 1]);
-            fftMag[w + 1] = std::sqrt(fftOut[i + 2] * fftOut[i + 2] + fftOut[i + 3] * fftOut[i + 3]);
-            fftMag[w + 2] = std::sqrt(fftOut[i + 4] * fftOut[i + 4] + fftOut[i + 5] * fftOut[i + 5]);
-            fftMag[w + 3] = std::sqrt(fftOut[i + 6] * fftOut[i + 6] + fftOut[i + 7] * fftOut[i + 7]);
+            fftMag[i + 0] = std::sqrt(fftOut[n + 0] * fftOut[n + 0] + fftOut[n + 1] * fftOut[n + 1]);
+            fftMag[i + 1] = std::sqrt(fftOut[n + 2] * fftOut[n + 2] + fftOut[n + 3] * fftOut[n + 3]);
+            fftMag[i + 2] = std::sqrt(fftOut[n + 4] * fftOut[n + 4] + fftOut[n + 5] * fftOut[n + 5]);
+            fftMag[i + 3] = std::sqrt(fftOut[n + 6] * fftOut[n + 6] + fftOut[n + 7] * fftOut[n + 7]);
          }
 #endif
 
