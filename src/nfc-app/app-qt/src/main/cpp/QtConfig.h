32b5e22a361e9cdf97db336880e2355fa7e0ebe6
Jose Vicente Campos <josevcm@gmail.com>
2022-02-13 13:39:56 +0100

    CMAKE installer generation

--- src/nfc-app/app-qt/src/main/cpp/QtConfig.h
+++ src/nfc-app/app-qt/src/main/cpp/QtConfig.h
@@ -3,6 +3,6 @@
  */
 #define NFC_LAB_VERSION_MAJOR 2
 #define NFC_LAB_VERSION_MINOR 6
-#define NFC_LAB_VERSION_PATCH 7
-#define NFC_LAB_VERSION_STRING "2.6.7"
-#define NFC_LAB_VENDOR_STRING "NFC laboratory 2.6.7, Jose Vicente Campos Martinez - josevcm@gmail.com"
+#define NFC_LAB_VERSION_PATCH 8
+#define NFC_LAB_VERSION_STRING "2.6.8"
+#define NFC_LAB_VENDOR_STRING "NFC laboratory 2.6.8, Jose Vicente Campos Martinez - josevcm@gmail.com"
