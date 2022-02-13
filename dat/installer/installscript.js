52a1f72d15357d0ac71b358ece46ecdd84b0fe9a
Jose Vicente Campos <josevcm@gmail.com>
2022-02-13 18:14:00 +0100

    CMAKE installer generation

--- dat/installer/installscript.js
+++ dat/installer/installscript.js
@@ -5,5 +5,4 @@ function Component()
 Component.prototype.createOperations = function() {
     component.createOperations();
     component.addOperation("CreateShortcut", "@TargetDir@/nfc-lab.exe", "@StartMenuDir@/NFC-Laboratory.lnk", "workingDirectory=@TargetDir@");
-    component.addOperation("CreateDesktopShortcut", "@TargetDir@/nfc-lab.exe", "@DesktopDir@/NFC-Laboratory.lnk", "workingDirectory=@TargetDir@");
 }
