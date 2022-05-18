3b61baafccfb2bbfa5698f6ff29fcdac5a62b0b4
Jose Vicente Campos <josevcm@gmail.com>
2022-05-18 20:43:12 +0200

    change version to 2.7.2

--- src/nfc-app/app-qt/src/main/cpp/QtConfig.h
+++ src/nfc-app/app-qt/src/main/cpp/QtConfig.h
@@ -3,6 +3,6 @@
  */
 #define NFC_LAB_VERSION_MAJOR 2
 #define NFC_LAB_VERSION_MINOR 7
-#define NFC_LAB_VERSION_PATCH 1
-#define NFC_LAB_VERSION_STRING "2.7.1"
-#define NFC_LAB_VENDOR_STRING "NFC laboratory 2.7.1, Jose Vicente Campos Martinez - josevcm@gmail.com"
+#define NFC_LAB_VERSION_PATCH 2
+#define NFC_LAB_VERSION_STRING "2.7.2"
+#define NFC_LAB_VENDOR_STRING "NFC laboratory 2.7.2, Jose Vicente Campos Martinez - josevcm@gmail.com"
