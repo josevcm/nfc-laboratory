8d46ebedc4de148528bd267283856b156b1583bb
jvcampos <jvcampos@indra.es>
2022-05-29 19:54:51 +0200

    prepare release version 2.8.0

--- src/nfc-app/app-qt/src/main/cpp/QtConfig.h
+++ src/nfc-app/app-qt/src/main/cpp/QtConfig.h
@@ -2,7 +2,7 @@
  * Auto-generated file
  */
 #define NFC_LAB_VERSION_MAJOR 2
-#define NFC_LAB_VERSION_MINOR 7
-#define NFC_LAB_VERSION_PATCH 3
-#define NFC_LAB_VERSION_STRING "2.7.3"
-#define NFC_LAB_VENDOR_STRING "NFC laboratory 2.7.3 - josevcm@gmail.com"
+#define NFC_LAB_VERSION_MINOR 8
+#define NFC_LAB_VERSION_PATCH 0
+#define NFC_LAB_VERSION_STRING "2.8.0"
+#define NFC_LAB_VENDOR_STRING "NFC laboratory 2.8.0 - josevcm@gmail.com"
