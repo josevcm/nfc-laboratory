29a1ae97c9d7a239315878836a411c7c5e5d05ed
jvcampos <jvcampos@indra.es>
2022-05-29 09:25:08 +0200

    change to develop version 2.7.3

--- src/nfc-app/app-qt/src/main/cpp/QtConfig.h
+++ src/nfc-app/app-qt/src/main/cpp/QtConfig.h
@@ -3,6 +3,6 @@
  */
 #define NFC_LAB_VERSION_MAJOR 2
 #define NFC_LAB_VERSION_MINOR 7
-#define NFC_LAB_VERSION_PATCH 2
-#define NFC_LAB_VERSION_STRING "2.7.2"
-#define NFC_LAB_VENDOR_STRING "NFC laboratory 2.7.2 - josevcm@gmail.com"
+#define NFC_LAB_VERSION_PATCH 3
+#define NFC_LAB_VERSION_STRING "2.7.3"
+#define NFC_LAB_VENDOR_STRING "NFC laboratory 2.7.3 - josevcm@gmail.com"
