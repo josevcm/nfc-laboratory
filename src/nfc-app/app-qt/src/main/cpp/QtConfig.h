8ad796f173b6e9738db9a2b7542fbb069dbafb99
Campos Martinez, Jose Vicente <jvcampos@indra.es>
2023-01-29 10:21:55 +0100

    tag version 2.8.3

--- src/nfc-app/app-qt/src/main/cpp/QtConfig.h
+++ src/nfc-app/app-qt/src/main/cpp/QtConfig.h
@@ -3,6 +3,6 @@
  */
 #define NFC_LAB_VERSION_MAJOR 2
 #define NFC_LAB_VERSION_MINOR 8
-#define NFC_LAB_VERSION_PATCH 2
-#define NFC_LAB_VERSION_STRING "2.8.2"
-#define NFC_LAB_VENDOR_STRING "NFC laboratory 2.8.2 - josevcm@gmail.com"
+#define NFC_LAB_VERSION_PATCH 3
+#define NFC_LAB_VERSION_STRING "2.8.3"
+#define NFC_LAB_VENDOR_STRING "NFC laboratory 2.8.3 - josevcm@gmail.com"
