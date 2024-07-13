741f9981af4b3fc0f4a4aa33b47150d322d02e42
Jose Vicente Campos <josevcm@gmail.com>
2024-07-13 10:25:08 +0200

    nfc-rx tool

--- src/nfc-test/app-rx/src/main/cpp/main.cpp
+++ src/nfc-test/app-rx/src/main/cpp/main.cpp
@@ -22,23 +22,29 @@
 
 */
 
+#ifdef __WIN32
+
+#include <windows.h>
+
+#endif
+
 #include <unistd.h>
 
 #include <mutex>
 #include <condition_variable>
+#include <iostream>
 
 #include <rt/Executor.h>
 #include <rt/Subject.h>
 #include <rt/Event.h>
+#include <rt/Logger.h>
 
-#include <sdr/SignalType.h>
-#include <sdr/AirspyDevice.h>
-
+#include <nfc/Nfc.h>
 #include <nfc/NfcFrame.h>
-#include <nfc/NfcDecoder.h>
 
 #include <nfc/FrameDecoderTask.h>
 #include <nfc/SignalReceiverTask.h>
+#include <nfc/SignalRecorderTask.h>
 
 #include <nlohmann/json.hpp>
 
@@ -46,20 +52,48 @@ using json = nlohmann::json;
 
 struct Main
 {
+   rt::Logger log {"Main"};
+
+   std::map<unsigned int, std::string> frameType {
+         {nfc::FrameType::CarrierOff,  "CarrierOff"},
+         {nfc::FrameType::CarrierOn,   "CarrierOn"},
+         {nfc::FrameType::PollFrame,   "PCD->PICC"},
+         {nfc::FrameType::ListenFrame, "PICC->PCD"}
+   };
+
+   std::map<unsigned int, std::string> frameTech {
+         {nfc::TechType::None, "None"},
+         {nfc::TechType::NfcA, "NfcA"},
+         {nfc::TechType::NfcB, "NfcB"},
+         {nfc::TechType::NfcF, "NfcD"},
+         {nfc::TechType::NfcV, "NfcV"}
+   };
+
+   std::map<unsigned int, std::string> frameRate {
+         {nfc::RateType::r106k, "106"},
+         {nfc::RateType::r212k, "212"},
+         {nfc::RateType::r424k, "424"},
+         {nfc::RateType::r848k, "848"},
+   };
+
    std::mutex mutex;
    std::condition_variable sync;
    std::atomic_bool terminate = false;
 
+   // create executor service
+   rt::Executor executor {128, 10};
+
    // streams subjects
-   rt::Subject<rt::Event> *decoderStatusStream = nullptr;
    rt::Subject<rt::Event> *receiverStatusStream = nullptr;
-   rt::Subject<rt::Event> *decoderCommandStream = nullptr;
    rt::Subject<rt::Event> *receiverCommandStream = nullptr;
+   rt::Subject<rt::Event> *decoderStatusStream = nullptr;
+   rt::Subject<rt::Event> *decoderCommandStream = nullptr;
    rt::Subject<nfc::NfcFrame> *decoderFrameStream = nullptr;
 
    // streams subscriptions
-   rt::Subject<rt::Event>::Subscription decoderStatusSubscription;
    rt::Subject<rt::Event>::Subscription receiverStatusSubscription;
+   rt::Subject<rt::Event>::Subscription recorderStatusSubscription;
+   rt::Subject<rt::Event>::Subscription decoderStatusSubscription;
    rt::Subject<nfc::NfcFrame>::Subscription decoderFrameSubscription;
 
    // receiver parameters
@@ -72,15 +106,28 @@ struct Main
    int biasTee = 0;
 
    bool nfcaEnabled = true;
-   bool nfcbEnabled = true;
-   bool nfcfEnabled = true;
-   bool nfcvEnabled = true;
+   bool nfcbEnabled = false;
+   bool nfcfEnabled = false;
+   bool nfcvEnabled = false;
 
    std::string decoderStatus = "idle";
    std::string receiverStatus = "idle";
 
+   bool debugEnabled = false;
+   bool verboseEnabled = false;
+
    Main()
    {
+      log.info("NFC laboratory, 2024 Jose Vicente Campos Martinez");
+   }
+
+   void init()
+   {
+      // create processing tasks
+      executor.submit(nfc::FrameDecoderTask::construct());
+      executor.submit(nfc::SignalReceiverTask::construct());
+      executor.submit(nfc::SignalRecorderTask::construct());
+
       // create receiver streams
       receiverStatusStream = rt::Subject<rt::Event>::name("receiver.status");
       receiverCommandStream = rt::Subject<rt::Event>::name("receiver.command");
@@ -112,7 +159,7 @@ struct Main
       {
          auto receiver = json::parse(data.value());
 
-         fprintf(stderr, "receiver status: %s\n", data.value().c_str());
+         log.info("receiver status: {}", {data.value()});
 
          // update receiver status
          receiverStatus = receiver["status"];
@@ -120,7 +167,7 @@ struct Main
          // if no receiver detected, finish...
          if (receiverStatus == "absent")
          {
-            fprintf(stderr, "no receiver found!\n");
+            log.info("no receiver found!");
             finish();
             return;
          }
@@ -151,17 +198,14 @@ struct Main
          // send configuration update
          if (!config.empty())
          {
-            fprintf(stderr, "Set receiver configuration: %s\n", config.dump().c_str());
+            log.info("set receiver configuration: {}", {config.dump()});
             receiverCommandStream->next({nfc::SignalReceiverTask::Configure, nullptr, nullptr, {{"data", config.dump()}}});
          }
 
             // if receiver is configured, start decoding
-         else if (decoderStatus == "idle")
+         else if (receiverStatus == "idle")
          {
-            decoderCommandStream->next({nfc::FrameDecoderTask::Start, [=]() {
-               receiverCommandStream->next({nfc::SignalReceiverTask::Start, [=]() {
-               }});
-            }});
+            receiverCommandStream->next({nfc::SignalReceiverTask::Start});
          }
       }
    }
@@ -172,7 +216,7 @@ struct Main
       {
          auto decoder = json::parse(data.value());
 
-         fprintf(stderr, "decoder status: %s\n", data.value().c_str());
+         log.info("decoder status: {}", {data.value()});
 
          // update receiver status
          decoderStatus = decoder["status"];
@@ -181,6 +225,9 @@ struct Main
 
          if (decoder["sampleRate"] != sampleRate)
             config["sampleRate"] = sampleRate;
+
+         if (decoder["debugEnabled"] != debugEnabled)
+            config["debugEnabled"] = debugEnabled;
 
          if (decoder["nfca"]["enabled"] != nfcaEnabled)
             config["nfca"]["enabled"] = nfcaEnabled;
@@ -197,74 +244,159 @@ struct Main
          // send configuration update
          if (!config.empty())
          {
-            fprintf(stderr, "Set decoder configuration: %s\n", config.dump().c_str());
+            log.info("set decoder configuration: {}", {config.dump()});
             decoderCommandStream->next({nfc::FrameDecoderTask::Configure, nullptr, nullptr, {{"data", config.dump()}}});
+         }
+            // if decoder is configured, start decoding
+         else if (decoderStatus == "idle")
+         {
+            decoderCommandStream->next({nfc::FrameDecoderTask::Start});
          }
       }
    }
 
    void handleDecoderFrame(const nfc::NfcFrame &frame)
    {
-      fprintf(stderr, "frame type %s\n", frame.frameType());
+      int offset = 0;
+      char buffer[16384];
 
-      if (frame.isPollFrame())
+      // add datagram time
+      offset += snprintf(buffer + offset, sizeof(buffer), "%010.3f ", frame.timeStart());
+
+      // add frame type
+      offset += snprintf(buffer + offset, sizeof(buffer), "(%s) ", frameType[frame.frameType()].c_str());
+
+      // data frames
+      if (frame.isPollFrame() || frame.isListenFrame())
       {
-         fprintf(stderr, "%4.3f >> \n", frame.timeStart());
+         // add tech type
+         offset += snprintf(buffer + offset, sizeof(buffer), "[%s@%.0f]: ", frameTech[frame.techType()].c_str(), roundf(float(frame.frameRate()) / 1000.0f));
+
+         // add data as HEX string
+         for (int i = 0; i < frame.size(); i++)
+         {
+            offset += snprintf(buffer + offset, sizeof(buffer) - offset, "%02X ", (unsigned int) frame[i]);
+         }
       }
-      else if (frame.isListenFrame())
-      {
-         fprintf(stderr, "%4.3f << \n", frame.timeStart());
-      }
+
+      // send to stdout
+      fprintf(stdout, "%s\n", buffer);
    }
 
    void finish()
    {
+      // shutdown all tasks
+      executor.shutdown();
+
+      // shutdown main loop
       terminate = true;
+
+      // notify
       sync.notify_all();
-   }
-
-   // no reentrant!
-   const char *hex(const nfc::NfcFrame &frame)
-   {
-      int offset = 0;
-      static char buffer[4096];
-
-      for (int i = 0; i < frame.size(); i++)
-      {
-         offset += snprintf(buffer + offset, sizeof(buffer) - offset, "%02X ", (unsigned int) frame[i]);
-      }
-
-      return buffer;
    }
 
    int run(int argc, char *argv[])
    {
+      int nsecs = -1;
+      int opt;
+
+      while ((opt = getopt(argc, argv, "vdp:t:")) != -1)
+      {
+         switch (opt)
+         {
+            case 'v':
+            {
+               rt::Logger::setWriterLevel(rt::Logger::DEBUG_LEVEL);
+               break;
+            }
+
+            case 'd':
+            {
+               debugEnabled = true;
+               break;
+            }
+
+            case 't':
+            {
+               char *endptr = nullptr;
+
+               nsecs = strtol(optarg, &endptr, 10);
+
+               if (endptr == optarg)
+               {
+                  log.error("invalid argument: {}", {optarg});
+                  return 1;
+               }
+
+               break;
+            }
+
+            default: /* '?' */
+               printf("Usage: [-v] [-d] [-p nfca,nfcb,nfcf,nfcv] [-t nsecs]\n");
+               printf("\tv: verbose mode, write logging information to stderr\n");
+               printf("\td: debug mode, write WAV file with raw decoding signals (highly affected performance!)\n");
+               printf("\tp: enable protocols, by default all are enabled\n");
+               printf("\tt: stop capture after number of seconds\n");
+               return -1;
+         }
+      }
+
+      // get start time
+      auto start = std::chrono::steady_clock::now();
+
+      // initialize
+      init();
+
       // wait until capture finished
       while (!terminate)
       {
          std::unique_lock<std::mutex> lock(mutex);
 
          sync.wait_for(lock, std::chrono::milliseconds(500));
+
+         if (nsecs > 0 && (std::chrono::steady_clock::now() - start) > std::chrono::seconds(nsecs))
+         {
+            finish();
+         }
       }
 
       return 0;
    }
-};
 
+} app;
+
+#ifdef __WIN32
+
+WINBOOL intHandler(DWORD sig)
+{
+   fprintf(stderr, "Terminate on signal %lu\n", sig);
+   app.finish();
+   return true;
+}
+
+#else
+void intHandler(int sig)
+{
+   fprintf(stderr, "Terminate on signal %d\n", sig);
+   app.finish();
+}
+#endif
 
 int main(int argc, char *argv[])
 {
-   fprintf(stderr, "NFC laboratory, 2024 Jose Vicente Campos Martinez\n");
+   // send logging events to stderr
+   rt::Logger::init(std::cerr);
 
-   // create executor service
-   rt::Executor executor(128, 10);
+   // disable logging at all (can be enabled with -v option)
+   rt::Logger::setWriterLevel(rt::Logger::NONE_LEVEL);
 
-   // create processing tasks
-   executor.submit(nfc::FrameDecoderTask::construct());
-   executor.submit(nfc::SignalReceiverTask::construct());
-
-   // create app instance
-   Main app;
+   // register signals
+#ifdef __WIN32
+   SetConsoleCtrlHandler(intHandler, TRUE);
+#else
+   signal(SIGINT, intHandler);
+   signal(SIGTERM, intHandler);
+#endif
 
    // and run
    return app.run(argc, argv);
