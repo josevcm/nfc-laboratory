3e95aa979a67c7591f832d97cb4fedeb3577526e
Jose Vicente Campos <josevcm@gmail.com>
2024-07-14 21:20:48 +0200

    add rtlsdr support

--- src/nfc-test/app-rx/src/main/cpp/main.cpp
+++ src/nfc-test/app-rx/src/main/cpp/main.cpp
@@ -44,7 +44,6 @@
 
 #include <nfc/FrameDecoderTask.h>
 #include <nfc/SignalReceiverTask.h>
-#include <nfc/SignalRecorderTask.h>
 
 #include <nlohmann/json.hpp>
 
@@ -54,6 +53,7 @@ struct Main
 {
    rt::Logger log {"Main"};
 
+   // frame type catalog
    std::map<unsigned int, std::string> frameType {
          {nfc::FrameType::CarrierOff,  "CarrierOff"},
          {nfc::FrameType::CarrierOn,   "CarrierOn"},
@@ -61,6 +61,7 @@ struct Main
          {nfc::FrameType::ListenFrame, "PICC->PCD"}
    };
 
+   // frame tech catalog
    std::map<unsigned int, std::string> frameTech {
          {nfc::TechType::None, "None"},
          {nfc::TechType::NfcA, "NfcA"},
@@ -69,6 +70,7 @@ struct Main
          {nfc::TechType::NfcV, "NfcV"}
    };
 
+   // frame rate catalog
    std::map<unsigned int, std::string> frameRate {
          {nfc::RateType::r106k, "106"},
          {nfc::RateType::r212k, "212"},
@@ -76,12 +78,31 @@ struct Main
          {nfc::RateType::r848k, "848"},
    };
 
+   // default receiver parameters for rtlsdr device
+   json rtlsdrReceiverParams = {
+         {"centerFreq",     27120000},
+         {"sampleRate",     3200000},
+         {"gainMode",       1},
+         {"gainValue",      77},
+         {"directSampling", 0}
+   };
+
+   // default receiver parameters for airspy device
+   json airspyReceiverParams = {
+         {"centerFreq", 40680000},
+         {"sampleRate", 10000000},
+         {"gainMode",   1},
+         {"gainValue",  3},
+         {"mixerAgc",   0},
+         {"biasTee",    0}
+   };
+
    std::mutex mutex;
    std::condition_variable sync;
    std::atomic_bool terminate = false;
 
    // create executor service
-   rt::Executor executor {128, 10};
+   rt::Executor executor {1, 4};
 
    // streams subjects
    rt::Subject<rt::Event> *receiverStatusStream = nullptr;
@@ -96,24 +117,21 @@ struct Main
    rt::Subject<rt::Event>::Subscription decoderStatusSubscription;
    rt::Subject<nfc::NfcFrame>::Subscription decoderFrameSubscription;
 
-   // receiver parameters
-   int centerFreq = 40680000;
-   int sampleRate = 10000000;
-   int directSampling = 0;
-   int gainMode = 1;
-   int gainValue = 3;
-   int mixerAgc = 0;
-   int biasTee = 0;
+   // current decoder status
+   bool decoderConfigured = false;
+   json decoderStatus {};
+   json decoderParams {
+         {"debugEnabled", false},
+         {"nfca",         {{"enabled", true}}},
+         {"nfcb",         {{"enabled", false}}},
+         {"nfcf",         {{"enabled", true}}},
+         {"nfcv",         {{"enabled", true}}}
+   };
 
-   bool nfcaEnabled = true;
-   bool nfcbEnabled = true;
-   bool nfcfEnabled = true;
-   bool nfcvEnabled = true;
-
-   std::string decoderStatus = "idle";
-   std::string receiverStatus = "idle";
-
-   bool debugEnabled = false;
+   // current receiver status
+   bool receiverConfigured = false;
+   json receiverStatus {};
+   json receiverParams {};
 
    Main()
    {
@@ -125,7 +143,6 @@ struct Main
       // create processing tasks
       executor.submit(nfc::FrameDecoderTask::construct());
       executor.submit(nfc::SignalReceiverTask::construct());
-      executor.submit(nfc::SignalRecorderTask::construct());
 
       // create receiver streams
       receiverStatusStream = rt::Subject<rt::Event>::name("receiver.status");
@@ -138,120 +155,18 @@ struct Main
 
       // handler for decoder status events
       receiverStatusSubscription = receiverStatusStream->subscribe([&](const rt::Event &event) {
-         handleReceiverStatus(event);
+         receiverStatus = json::parse(event.get<std::string>("data").value());
       });
 
       // subscribe to decoder status
       decoderStatusSubscription = decoderStatusStream->subscribe([&](const rt::Event &event) {
-         handleDecoderStatus(event);
+         decoderStatus = json::parse(event.get<std::string>("data").value());
       });
 
       // subscribe to decoder frames
       decoderFrameSubscription = decoderFrameStream->subscribe([&](const nfc::NfcFrame &frame) {
          handleDecoderFrame(frame);
       });
-   }
-
-   void handleReceiverStatus(const rt::Event &event)
-   {
-      if (auto data = event.get<std::string>("data"))
-      {
-         auto receiver = json::parse(data.value());
-
-         log.info("receiver status: {}", {data.value()});
-
-         // update receiver status
-         receiverStatus = receiver["status"];
-
-         // if no receiver detected, finish...
-         if (receiverStatus == "absent")
-         {
-            log.info("no receiver found!");
-            finish();
-            return;
-         }
-
-         json config;
-
-         if (receiver["centerFreq"] != centerFreq)
-            config["centerFreq"] = centerFreq;
-
-         if (receiver["sampleRate"] != sampleRate)
-            config["sampleRate"] = sampleRate;
-
-         if (receiver["directSampling"] != directSampling)
-            config["directSampling"] = directSampling;
-
-         if (receiver["biasTee"] != biasTee)
-            config["biasTee"] = biasTee;
-
-         if (receiver["mixerAgc"] != mixerAgc)
-            config["mixerAgc"] = mixerAgc;
-
-         if (receiver["gainMode"] != gainMode || receiver["gainValue"] != gainValue)
-         {
-            config["gainMode"] = gainMode;
-            config["gainValue"] = gainValue;
-         }
-
-         // send configuration update
-         if (!config.empty())
-         {
-            log.info("set receiver configuration: {}", {config.dump()});
-            receiverCommandStream->next({nfc::SignalReceiverTask::Configure, nullptr, nullptr, {{"data", config.dump()}}});
-         }
-
-            // if receiver is configured, start decoding
-         else if (receiverStatus == "idle")
-         {
-            receiverCommandStream->next({nfc::SignalReceiverTask::Start});
-         }
-      }
-   }
-
-   void handleDecoderStatus(const rt::Event &event)
-   {
-      if (auto data = event.get<std::string>("data"))
-      {
-         auto decoder = json::parse(data.value());
-
-         log.info("decoder status: {}", {data.value()});
-
-         // update receiver status
-         decoderStatus = decoder["status"];
-
-         json config;
-
-         if (decoder["sampleRate"] != sampleRate)
-            config["sampleRate"] = sampleRate;
-
-         if (decoder["debugEnabled"] != debugEnabled)
-            config["debugEnabled"] = debugEnabled;
-
-         if (decoder["nfca"]["enabled"] != nfcaEnabled)
-            config["nfca"]["enabled"] = nfcaEnabled;
-
-         if (decoder["nfcb"]["enabled"] != nfcbEnabled)
-            config["nfcb"]["enabled"] = nfcbEnabled;
-
-         if (decoder["nfcf"]["enabled"] != nfcfEnabled)
-            config["nfcf"]["enabled"] = nfcfEnabled;
-
-         if (decoder["nfcv"]["enabled"] != nfcvEnabled)
-            config["nfcv"]["enabled"] = nfcvEnabled;
-
-         // send configuration update
-         if (!config.empty())
-         {
-            log.info("set decoder configuration: {}", {config.dump()});
-            decoderCommandStream->next({nfc::FrameDecoderTask::Configure, nullptr, nullptr, {{"data", config.dump()}}});
-         }
-            // if decoder is configured, start decoding
-         else if (decoderStatus == "idle")
-         {
-            decoderCommandStream->next({nfc::FrameDecoderTask::Start});
-         }
-      }
    }
 
    void handleDecoderFrame(const nfc::NfcFrame &frame)
@@ -282,6 +197,99 @@ struct Main
       fprintf(stdout, "%s\n", buffer);
    }
 
+   int checkReceiverStatus()
+   {
+      std::string name = receiverStatus["name"];
+
+      // if no receiver detected, finish...
+      if (receiverStatus["status"] == "absent")
+      {
+         log.info("no receiver found!");
+         return -1;
+      }
+
+      // update decoder sample rate
+      if (receiverStatus["sampleRate"].is_number())
+         decoderParams["sampleRate"] = receiverStatus["sampleRate"];
+
+      // check receiver parameters
+      json params;
+      json config;
+
+      // default parameters for AirSpy
+      if (name.find("airspy") == 0)
+      {
+         params = airspyReceiverParams;
+      }
+         // default parameters for Rtl SDR
+      else if (name.find("rtlsdr") == 0)
+      {
+         params = rtlsdrReceiverParams;
+      }
+      else
+      {
+         log.error("unknown receiver: {}", {name});
+         return -1;
+      }
+
+      for (auto &param: params.items())
+      {
+         if (receiverStatus[param.key()] != param.value())
+         {
+            config[param.key()] = param.value();
+         }
+      }
+
+      // if no configuration needed, just start receiver
+      receiverConfigured = config.empty();
+
+      // send configuration update
+      if (!receiverConfigured)
+      {
+         log.info("set receiver configuration: {}", {config.dump()});
+         receiverCommandStream->next({nfc::SignalReceiverTask::Configure, [=]() { receiverConfigured = true; }, nullptr, {{"data", config.dump()}}});
+      }
+
+      if (receiverConfigured && receiverStatus["status"] == "idle")
+      {
+         receiverStatus["status"] = "waiting";
+         receiverCommandStream->next({nfc::SignalReceiverTask::Start});
+      }
+
+      return 0;
+   }
+
+   int checkDecoderStatus()
+   {
+      json config;
+
+      for (auto &param: decoderParams.items())
+      {
+         if (decoderStatus[param.key()] != param.value())
+         {
+            config[param.key()] = param.value();
+         }
+      }
+
+      // if no configuration needed, just start decoder
+      decoderConfigured = config.empty();
+
+      // send configuration update
+      if (!decoderConfigured)
+      {
+         log.info("set decoder configuration: {}", {config.dump()});
+         decoderCommandStream->next({nfc::FrameDecoderTask::Configure, [=]() { decoderConfigured = true; }, nullptr, {{"data", config.dump()}}});
+      }
+
+      if (decoderConfigured && decoderStatus["status"] == "idle")
+      {
+         decoderStatus["status"] = "waiting";
+         decoderCommandStream->next({nfc::FrameDecoderTask::Start});
+      }
+
+      return 0;
+   }
+
    void finish()
    {
       // shutdown all tasks
@@ -296,58 +304,61 @@ struct Main
 
    int run(int argc, char *argv[])
    {
-      int nsecs = -1;
       int opt;
+      int nsecs = -1;
+      char *endptr = nullptr;
 
       while ((opt = getopt(argc, argv, "vdp:t:")) != -1)
       {
          switch (opt)
          {
+            // enable verbose mode
             case 'v':
             {
-               rt::Logger::setWriterLevel(rt::Logger::DEBUG_LEVEL);
+               if (rt::Logger::getWriterLevel() < rt::Logger::INFO_LEVEL)
+                  rt::Logger::setWriterLevel(rt::Logger::INFO_LEVEL);
+               else if (rt::Logger::getWriterLevel() < rt::Logger::TRACE_LEVEL)
+                  rt::Logger::setWriterLevel(rt::Logger::getWriterLevel() + 1);
+
                break;
             }
 
+               // enable signal debug mode
             case 'd':
             {
-               debugEnabled = true;
+               decoderParams["debugEnabled"] = true;
                break;
             }
 
+               // enable protocols
             case 'p':
             {
                std::string protocols = optarg;
-
-               nfcaEnabled = protocols.find("nfca") != std::string::npos;
-               nfcbEnabled = protocols.find("nfcb") != std::string::npos;
-               nfcfEnabled = protocols.find("nfcf") != std::string::npos;
-               nfcvEnabled = protocols.find("nfcv") != std::string::npos;
-
+               decoderParams["nfca"]["enabled"] = protocols.find("nfca") != std::string::npos;
+               decoderParams["nfcb"]["enabled"] = protocols.find("nfcb") != std::string::npos;
+               decoderParams["nfcf"]["enabled"] = protocols.find("nfcf") != std::string::npos;
+               decoderParams["nfcv"]["enabled"] = protocols.find("nfcv") != std::string::npos;
                break;
             }
 
+               // limit running time
             case 't':
             {
-               char *endptr = nullptr;
-
                nsecs = strtol(optarg, &endptr, 10);
 
                if (endptr == optarg)
                {
-                  log.error("invalid argument: {}", {optarg});
-                  return 1;
+                  printf("Invalid value for 't' argument\n");
+                  showUsage();
+                  return -1;
                }
 
                break;
             }
 
             default: /* '?' */
-               printf("Usage: [-v] [-d] [-p nfca,nfcb,nfcf,nfcv] [-t nsecs]\n");
-               printf("\tv: verbose mode, write logging information to stderr\n");
-               printf("\td: debug mode, write WAV file with raw decoding signals (highly affected performance!)\n");
-               printf("\tp: enable protocols, by default all are enabled\n");
-               printf("\tt: stop capture after number of seconds\n");
+               printf("Unknown option '%c'\n", (char) opt);
+               showUsage();
                return -1;
          }
       }
@@ -369,9 +380,28 @@ struct Main
          {
             finish();
          }
+
+         else if (checkReceiverStatus() < 0)
+         {
+            finish();
+         }
+
+         else if (checkDecoderStatus() < 0)
+         {
+            finish();
+         }
       }
 
       return 0;
+   }
+
+   static void showUsage()
+   {
+      printf("Usage: [-v] [-d] [-p nfca,nfcb,nfcf,nfcv] [-t nsecs]\n");
+      printf("\tv: verbose mode, write logging information to stderr\n");
+      printf("\td: debug mode, write WAV file with raw decoding signals (highly affected performance!)\n");
+      printf("\tp: enable protocols, by default all are enabled\n");
+      printf("\tt: stop capture after number of seconds\n");
    }
 
 } app;
