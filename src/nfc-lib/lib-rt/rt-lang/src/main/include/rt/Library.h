9b491f60b0d58836357c32f8cb7594b891edc0f2
Jose Vicente Campos <josevcm@gmail.com>
2025-06-18 19:52:49 +0200

    Dynamic Library

---
+++ src/nfc-lib/lib-rt/rt-lang/src/main/include/rt/Library.h
@@ -0,0 +1,53 @@
+/*
+
+This file is part of NFC-LABORATORY.
+
+  Copyright (C) 2025 Jose Vicente Campos Martinez, <josevcm@gmail.com>
+
+  NFC-LABORATORY is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  NFC-LABORATORY is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with NFC-LABORATORY. If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+#ifndef LIBRARY_H
+#define LIBRARY_H
+
+#include <memory>
+
+namespace rt {
+
+// TODO: WORK IN PROGRESS...
+class Library
+{
+   struct Impl;
+
+   public:
+
+      Library(const std::string &name);
+
+      ~Library();
+
+      bool isLoaded() const;
+
+      template <typename T>
+      T resolve(const std::string &symbol);
+
+   private:
+
+      std::shared_ptr<Impl> impl;
+
+};
+
+}
+
+#endif //LIBRARY_H
